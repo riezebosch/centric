3f5d3d1f9f45d49bf08c15618f7c70f15a232744
Manuel Riezebosch <manuel.riezebosch@infosupport.com>
2015-11-18 13:47:38 +0100

    edmx gegenereerd op basis van bestaande database.

---
+++ Service.DataModel.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Service.DataModel.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var context = new SchoolEntities())
+            {
+                foreach (var person in context.People)
+                {
+                    Console.WriteLine("Name: {0} {1}", person.FirstName, person.LastName);
+                }
+            }
+        }
+    }
+}
