e3b5f7bbe2a428a04558ffaf1868a342dbe20db0
Manuel Riezebosch <manuel.riezebosch@infosupport.com>
2015-11-20 16:15:22 +0100

    appdomain

--- ReflectionDemo/Program.cs
+++ ReflectionDemo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -11,51 +12,64 @@ namespace ReflectionDemo
     {
         static void Main(string[] args)
         {
-            
-            if (args.Length != 1)
+
+            if (!args.Any())
             {
                 Console.WriteLine("Specify test assembly is parameter");
                 return;
             }
 
 
-            var location = args.First();
-            var ai = new AssemblyInspector(location);
-            var classes = ai.GetClasses();
-            int apassed = 0, afailed = 0;
-
-            foreach (var c in classes)
+            foreach (var location in args)
             {
-                var runner = new TestRunner(c);
-                Console.WriteLine(c.Name);
+                Console.WriteLine(location);
+                var setup = new AppDomainSetup();
+                setup.ConfigurationFile = location + ".config";
+                setup.ApplicationBase = Path.GetDirectoryName(location);
 
-                var ci = new ClassInspector(c);
+                Environment.CurrentDirectory = Path.GetDirectoryName(location);
+                var appdomain = AppDomain.CreateDomain("test", AppDomain.CurrentDomain.Evidence, setup);
+                appdomain.Load(Path.GetFileName(location));
+                
+                    
 
-                if (!ExecuteIfExists(runner, ci.GetClassInitialize()))
+                var ai = new AssemblyInspector(location);
+                var classes = ai.GetClasses();
+                int apassed = 0, afailed = 0;
+
+                foreach (var c in classes)
                 {
-                    break;
-                }
+                    var runner = (TestRunner)appdomain.CreateInstanceAndUnwrap(Assembly.GetExecutingAssembly().FullName, typeof(TestRunner).FullName, new object[] { c });
+                    Console.WriteLine(c.Name);
 
-                var testinit = ci.GetTestInitialize();
-                var testclean = ci.GetTestCleanUp();
-                var methods = ci.GetMethods();
-                int cpassed = 0, cfailed = 0;
+                    var ci = new ClassInspector(c);
 
-                foreach (var m in methods)
-                {
-                    if (!ExecuteIfExists(runner, testinit))
+                    if (!ExecuteIfExists(runner, ci.GetClassInitialize()))
                     {
                         break;
                     }
 
-                    ExecuteTest(ref apassed, ref afailed, runner, ref cpassed, ref cfailed, m);
-                    ExecuteIfExists(runner, testclean);
-                }
+                    var testinit = ci.GetTestInitialize();
+                    var testclean = ci.GetTestCleanUp();
+                    var methods = ci.GetMethods();
+                    int cpassed = 0, cfailed = 0;
 
-                ExecuteIfExists(runner, ci.GetClassCleanUp());
-                Console.WriteLine("  [{0}] Tests passed: {1}, tests failed: {2}", c.Name, cpassed, cfailed);
+                    foreach (var m in methods)
+                    {
+                        if (!ExecuteIfExists(runner, testinit))
+                        {
+                            break;
+                        }
+
+                        ExecuteTest(ref apassed, ref afailed, runner, ref cpassed, ref cfailed, m);
+                        ExecuteIfExists(runner, testclean);
+                    }
+
+                    ExecuteIfExists(runner, ci.GetClassCleanUp());
+                    Console.WriteLine("  [{0}] Tests passed: {1}, tests failed: {2}", c.Name, cpassed, cfailed);
+                }
+                Console.WriteLine("Tests passed: {0}, tests failed: {1}", apassed, afailed);
             }
-            Console.WriteLine("Tests passed: {0}, tests failed: {1}", apassed, afailed);
         }
 
         private static bool ExecuteIfExists(TestRunner runner, MethodInfo m)
